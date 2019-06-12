bf0c89a73b376f689eefd1da479670a3dc5df382
Damien Stamates <damienstamates@northwesternmutual.com>
2019-06-12 17:27:48 -0500

    Stripped out old configuration functions

--- configuration.go
+++ configuration.go
@@ -15,32 +15,3 @@ func WithPredicateKey(predicateName string) ClientConfiguration {
 		c.predicateKey = predicateName
 	}
 }
-
-func WithQuirkName(name string) ClientConfiguration {
-	return func(c *Client) {
-		c.quirkName = name
-	}
-}
-
-func WithQuirkRel(relName string) ClientConfiguration {
-	return func(c *Client) {
-		c.quirkRel = relName
-	}
-}
-
-func UseUIDIncrementer() ClientConfiguration {
-	return func(c *Client) {
-		c.insertMode = incrementor
-	}
-}
-
-func UseReverseEdges() ClientConfiguration {
-	return func(c *Client) {
-		c.reverseEdge = true
-	}
-}
-
-func WithStartUID(i uint64) ClientConfiguration {
-	setStartUID(i)
-	return func(*Client) {}
-}
