9dd894ee641d9b2679613f16a53aa6a85cb23eea
Damien Stamates <thedoubledimensions@gmail.com>
2019-06-14 05:07:37 -0500

    Added comments to exported types

--- log.go
+++ log.go
@@ -5,14 +5,18 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
+// NewNilLogger returns the *logging.NilLogger logger which doesn't log anything.
 func NewNilLogger() logging.Logger {
 	return logging.NewNilLogger()
 }
 
+// NewCustomLogger returns a *logging.CustomLogger with the desired level
+// and encoder configuration that may be passed in.
 func NewCustomLogger(level []byte, config zapcore.EncoderConfig) logging.Logger {
 	return logging.NewCustomLogger(level, config)
 }
 
+// NewDebugLogger is a debug level zap logger that can be used when testing.
 func NewDebugLogger() logging.Logger {
 	return logging.NewDebugLogger()
 }
--- logging/customlogger.go
+++ logging/customlogger.go
@@ -7,7 +7,8 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-// CustomLogger
+// CustomLogger is a customizable logging.Logger where you can choose the level
+// and the zapcore encoder configuration.
 type CustomLogger struct {
 	logger *zap.Logger
 }
@@ -25,12 +26,13 @@ func NewCustomLogger(level []byte, config zapcore.EncoderConfig) *CustomLogger {
 	}
 }
 
+// Info logs at an info level.
 func (l *CustomLogger) Info(msg string, iFields ...interface{}) {
 	fields := interfaceToZapField(iFields...)
 	l.logger.Info(msg, fields...)
 }
 
-// Debug logs nothing.
+// Debug logs at an debug level.
 func (l *CustomLogger) Debug(msg string, iFields ...interface{}) {
 	fields := interfaceToZapField(iFields...)
 	l.logger.Debug(msg, fields...)
@@ -42,18 +44,20 @@ func (l *CustomLogger) Warn(msg string, iFields ...interface{}) {
 	l.logger.Warn(msg, fields...)
 }
 
-// Error logs nothing.
+// Error logs at an error level.
 func (l *CustomLogger) Error(msg string, iFields ...interface{}) {
 	fields := interfaceToZapField(iFields...)
 	l.logger.Error(msg, fields...)
 }
 
-// Fatal logs nothing.
+// Fatal logs at a fatal level and exits.
 func (l *CustomLogger) Fatal(msg string, iFields ...interface{}) {
 	fields := interfaceToZapField(iFields...)
 	l.logger.Fatal(msg, fields...)
 }
 
+// interfaceToZapField takes the interfaces passed in and type asserts them
+// into a zap.Field and returns a slice.
 func interfaceToZapField(iFields ...interface{}) (fields []zapcore.Field) {
 	for i := 0; i < len(iFields); i++ {
 		fields = append(fields, iFields[i].(zapcore.Field))
