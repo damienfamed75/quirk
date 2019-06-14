2d32930cf36708c46dbef57212cbf4ebcac95990
Damien Stamates <thedoubledimensions@gmail.com>
2019-06-14 01:23:06 -0500

    Added quick godoc comment in quirk.go

---
+++ quirk.go
@@ -0,0 +1,5 @@
+// Package quirk provides the quirk client which the user should interface with.
+//
+// All functionality is provided in this package and isn't really seperated out,
+// because the library isn't big enough.
+package quirk
