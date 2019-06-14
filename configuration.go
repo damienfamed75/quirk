dd408d508b4c3cd178750e6fd18f87976b2da70e
Damien Stamates <thedoubledimensions@gmail.com>
2019-06-14 01:46:34 -0500

    Cleaned up and tested configuration.go

--- configuration.go
+++ configuration.go
@@ -2,14 +2,20 @@ package quirk
 
 import "github.com/damienfamed75/quirk/logging"
 
+// ClientConfiguration is used to pass in options
+// to change the client and customize it to the user's liking.
 type ClientConfiguration func(*Client)
 
+// WithLogger sets the logger used by the quirk client.
+// By default this is quirk.NewNilLogger.
 func WithLogger(l logging.Logger) ClientConfiguration {
 	return func(c *Client) {
 		c.logger = l
 	}
 }
 
+// WithPredicateKey sets the field(predicate) that will
+// be used to label inserted nodes. By default this is "name"
 func WithPredicateKey(predicateName string) ClientConfiguration {
 	return func(c *Client) {
 		c.predicateKey = predicateName
---
+++ configuration_test.go
@@ -0,0 +1,43 @@
+package quirk
+
+import (
+	"testing"
+
+	. "github.com/franela/goblin"
+)
+
+func BenchmarkWithLogger(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = WithLogger(NewDebugLogger())
+	}
+}
+
+func BenchmarkWithPredicateKey(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = WithPredicateKey("key")
+	}
+}
+
+func TestWithLogger(t *testing.T) {
+	g := Goblin(t)
+
+	g.Describe("Logger", func() {
+		dl := NewDebugLogger()
+		g.It("should equal a debug logger", func() {
+			g.Assert(NewClient(WithLogger(dl)).logger).
+				Equal(dl)
+		})
+	})
+}
+
+func TestWithPredicateKey(t *testing.T) {
+	g := Goblin(t)
+
+	g.Describe("Predicate key", func() {
+		key := "customKey"
+		g.It("should equal customKey", func() {
+			g.Assert(NewClient(WithPredicateKey(key)).predicateKey).
+				Equal(key)
+		})
+	})
+}
