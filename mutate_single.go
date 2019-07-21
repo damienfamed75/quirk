d65deddd5c0f9b55c6fdedbbc88fd351dd020a3a
Damien Stamates <thedoubledimensions@gmail.com>
2019-07-21 02:44:40 -0500

    update reflect calls to use the client receiver

--- mutate_single.go
+++ mutate_single.go
@@ -8,7 +8,7 @@ import (
 func (c *Client) mutateSingleStruct(ctx context.Context, dg DgraphClient,
 	d interface{}, uidMap map[string]string, m *sync.Mutex) (bool, error) {
 	// Use reflect to package the predicate and values in slices.
-	predVals := reflectMaps(d)
+	predVals := c.reflectMaps(d)
 
 	return c.mutate(ctx, dg, predVals, uidMap, m)
 }
