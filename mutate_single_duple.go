f0c80d8db183d78a57b3d70cc9bf72feaf1ef89b
Damien Stamates <thedoubledimensions@gmail.com>
2019-07-21 02:45:08 -0500

    Finished the mutate single duple function

--- mutate_single_duple.go
+++ mutate_single_duple.go
@@ -8,15 +8,5 @@ import (
 func (c *Client) mutateSingleDupleNode(ctx context.Context, dg DgraphClient,
 	node *DupleNode, uidMap map[string]string, m sync.Locker) (bool, error) {
 
-	// predVals := reflectDupleMaps(node)
-
-	return false, nil
-}
-
-func reflectDupleMaps(node *DupleNode) predValPairs {
-	// Maybe replace all instances of predValDat+predValPairs with DupleNode
-	// Maybe remove Name string because it's difficult to make?
-	// Or maybe keep it for the user?
-
-	return nil
+	return c.mutate(ctx, dg, node, uidMap, m)
 }
