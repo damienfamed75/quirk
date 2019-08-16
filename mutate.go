2306843daaff69bd22c3479f2c939b5b5dc6b887
Damien Stamates <thedoubledimensions@gmail.com>
2019-08-16 14:37:17 -0500

    Merged single mutate functions into one file

--- mutate.go
+++ mutate.go
@@ -5,6 +5,8 @@ import (
 	"sync"
 )
 
+// mutate is used to upsert a single node using Quirk. All single mutate
+// functions call this one to do the upsert.
 func (c *Client) mutate(ctx context.Context, dg DgraphClient,
 	d *DupleNode, uidMap map[string]UID, m sync.Locker) (bool, error) {
 
@@ -18,4 +20,42 @@ func (c *Client) mutate(ctx context.Context, dg DgraphClient,
 	m.Unlock()
 
 	return res.new, nil
+}
+
+// mutateSingleStruct uses reflect to create a DupleNode struct out of the given
+// structure. Once the DupleNode is made then it is passed to mutate to be upserted.
+func (c *Client) mutateSingleStruct(ctx context.Context, dg DgraphClient,
+	d interface{}, uidMap map[string]UID, m *sync.Mutex) (bool, error) {
+	// Use reflect to package the predicate and values in slices.
+	predVals := c.reflectMaps(d)
+
+	return c.mutate(ctx, dg, predVals, uidMap, m)
+}
+
+// mutateSingleDupleNode passed the DupleNode given to mutate to be upserted.
+func (c *Client) mutateSingleDupleNode(ctx context.Context, dg DgraphClient,
+	node interface{}, uidMap map[string]UID, m *sync.Mutex) (bool, error) {
+
+	return c.mutate(ctx, dg, node.(*DupleNode), uidMap, m)
+}
+
+// mutateStringMap loops through the given map to create a DupleNode struct.
+// Once the DupleNode is made then it is passed to mutate to be upserted.
+func (c *Client) mutateStringMap(ctx context.Context, dg DgraphClient,
+	d map[string]string, uidMap map[string]UID, m sync.Locker) (bool, error) {
+	// Convert out map[string]string to usable predicate and value data.
+	predVals := c.mapToPredValPairs(d)
+
+	return c.mutate(ctx, dg, predVals, uidMap, m)
+}
+
+// mutateDynamicMap loops through the given map to create a DupleNode struct.
+// Once the DupleNode is made then it is passed to mutate to be upserted.
+// Note: The looping process supports multiple Dgraph datatypes.
+func (c *Client) mutateDynamicMap(ctx context.Context, dg DgraphClient,
+	d map[string]interface{}, uidMap map[string]UID, m sync.Locker) (bool, error) {
+	// Convert out map[string]interface{} to usable predicate and value data.
+	predVals := c.dynamicMapToPredValPairs(d)
+
+	return c.mutate(ctx, dg, predVals, uidMap, m)
 }
--- mutate_test.go
+++ mutate_test.go
@@ -9,6 +9,80 @@ import (
 	. "github.com/franela/goblin"
 )
 
+func TestMutateSingleStruct(t *testing.T) {
+	g := Goblin(t)
+	c := NewClient(WithPredicateKey("username"))
+
+	g.Describe("UIDMap, New, and Error", func() {
+		uidMap := make(map[string]UID)
+		ctx := context.Background()
+
+		g.It("should be empty, false, and nil", func() {
+			new, err := c.mutateSingleStruct(ctx,
+				dgo.NewDgraphClient(&testDgraphClient{queryResponse: testValidJSONOutput}),
+				&testPersonCorrect, uidMap, &sync.Mutex{})
+
+			g.Assert(new).
+				Equal(false)
+
+			g.Assert(err).
+				Equal(error(nil))
+		})
+
+		g.It("should be not empty, false, and nil when new", func() {
+			new, err := c.mutateSingleStruct(ctx,
+				dgo.NewDgraphClient(&testDgraphClient{
+					queryResponse: []byte("{}"),
+				}),
+				&testPersonCorrect, uidMap, &sync.Mutex{})
+
+			g.Assert(new).
+				Equal(true)
+
+			g.Assert(err).
+				Equal(error(nil))
+
+			g.Assert(len(uidMap)).
+				Equal(1)
+
+			g.Assert(uidMap["damienstamates"].uid).
+				Equal("0x1")
+		})
+	})
+}
+
+func TestMutateSingleDupleNode(t *testing.T) {
+	g := Goblin(t)
+	c := NewClient(WithPredicateKey("username"))
+
+	g.Describe("UIDMap, New, and Error", func() {
+		duple := &DupleNode{
+			Identifier: "damienstamates",
+			Duples:     []Duple{},
+		}
+
+		uidMap := make(map[string]UID)
+		ctx := context.Background()
+
+		g.It("should be empty, false, and nil", func() {
+			new, err := c.mutateSingleDupleNode(ctx,
+				dgo.NewDgraphClient(&testDgraphClient{
+					queryResponse: testValidJSONOutput,
+				}),
+				duple, uidMap, &sync.Mutex{})
+
+			g.Assert(new).
+				Equal(true)
+
+			g.Assert(err).
+				Equal(error(nil))
+
+			g.Assert(len(uidMap)).
+				Equal(1)
+		})
+	})
+}
+
 func TestStringMap(t *testing.T) {
 	g := Goblin(t)
 	c := NewClient(WithPredicateKey("username"))
